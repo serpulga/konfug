c5f9a4e9f6647090623054668d9af640a08e7ee5
Sergio Pulgarin <serpulga@gmail.com>
2020-06-15 21:08:00 -0400

    Bumps version to 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 setuptools.setup(
     name='konfug',
     py_modules=['konfug'],
-    version='0.0.1',
+    version='0.0.2',
     description='The configuration source for all your projects',
     author='Sergio Pulgarin',
     license='BSD',
